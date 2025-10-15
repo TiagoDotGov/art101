922814edb86d91d3904bb36e1e1b0760139ceba1
TiagoDotGov <gideonsilvatiago@gmail.com>
2025-10-14 18:49:56 -0700

    Exercises in class

---
+++ Exercises/ex04.js
@@ -0,0 +1,14 @@
+let totalNumber = 0;
+//add a button titled Try To Click Me
+$("#needy-button").click( function(){
+    totalNumber = totalNumber + 1;
+
+    $("#needy-button").html("You clicked me " + totalNumber);
+})
+//when button is clicked
+//when it is clicked
+//add one to the toal number
+//show the total number
+//on our button
+//show "clicked total number of times"
+//a top limit
