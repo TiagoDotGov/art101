334e885b542750c9f27224c53514874b8022ed17
TiagoDotGov <gideonsilvatiago@gmail.com>
2025-11-24 11:02:30 -0800

    refactoring code for Lab 9

--- Environ/env06.js
+++ Environ/env06.js
@@ -32,20 +32,43 @@ let hotSpring = {
 //create array of all the objects
 let characters = [oryx, macaque, tea, hotSpring]
 let charNames = ["Oryx", "The Monkey", "The Tea", "The Hot Spring"]
-//find button in flexbox
-$("#mover").click(function () {
-    //change contents of flexbox on click to cycle through the array
-    let reminder = count % characters.length;
-    let remTwo = count % charNames.length;
-    //change #descTitle to this text plus this indexed value in the array charNames
-    $("#descTitle").html("Meet " + charNames[remTwo] + "!");
-    //create a new array of just the object values from this indexed object in the array characters
-    let myArray = Object.values(characters[reminder])
-    //print the array as a string in the element #descContents
-    $("#descContents").html(myArray.toString());
 
+function fadeOutAnimation() {
+    $(this).stop();
+    $(this).fadeOut(300);
+};
+
+function fadeInAnimation() {
+    $(this).stop();
+    $(this).fadeIn(170)
+};
+
+function enterToSubmit(e) {
+    if (e.which === 13) {
+        let thrName = $("#nameThrall").val();
+        $("#thrallTitle").text(thrName);
+        $("#nameThrall").val("");
+    }
+};
+
+function moveThrall() {
+    $(document).mousemove(function (event) {
+        $("#thrallMover").css({
+            left: event.pageX - 270,
+            top: event.pageY - 300,
+            position: "absolute"
+        })
+    }
+    )
+};
+
+function printDescriptions() {
+    let reminder = count % characters.length;
+    $("#descTitle").html("Meet " + charNames[reminder] + "!");
+    let myArray = Object.values(characters[reminder])
+    $("#descContents").html(myArray.toString());
     count = count + 1
-});
+};
 
 function changeWeather(weather) {
     $("body").css("background-image", "url('images/" + weather + ".jpg')")
@@ -54,45 +77,76 @@ function hideMonkey() {
     if (countWeather == 1) { $("#monkeyGroup").css("display", "block") }
     else { $("#monkeyGroup").css("display", "none") }
 }
-//find button to click
-//when clicked, cycle weather (sunny, snowy)
-$("#weatherButton").click(function () {
+
+function weatherEvent (){
     countWeather = countWeather + 1;
     if (countWeather == 2) { countWeather = 0 };
     hideMonkey();
     changeWeather(states[countWeather]);
-})
+
+}
+
+$("#weatherButton").click(weatherEvent)
+//              Old code 
+//                  |
+//                  V
+//find button to click
+//when clicked, cycle weather (sunny, snowy)
+// $("#weatherButton").click(function () {
+//     countWeather = countWeather + 1;
+//     if (countWeather == 2) { countWeather = 0 };
+//     hideMonkey();
+//     changeWeather(states[countWeather]);
+// })
 //change parameter to the current weather
 //when parameter is snowy, set background to "snowy" picture, add monkey group
 //when the parameter is sunny, set background to the normal one, remove monkey group
-$("#takenking").hover(function () {
-    $(this).stop();
-    $(this).fadeOut(300);
-}, function () {
-    $(this).stop();
-    $(this).fadeIn(170)
-});
+
+$("#mover").click(printDescriptions);
+//              Old code 
+//                  |
+//                  V
+//find button in flexbox
+//$("#mover").click(function () {
+//change contents of flexbox on click to cycle through the array
+//let reminder = count % characters.length;
+// let remTwo = count % charNames.length;
+//change #descTitle to this text plus this indexed value in the array charNames
+//$("#descTitle").html("Meet " + charNames[reminder] + "!");
+//create a new array of just the object values from this indexed object in the array characters
+//let myArray = Object.values(characters[reminder])
+//print the array as a string in the element #descContents
+//$("#descContents").html(myArray.toString());
+//count = count + 1
+//});
+
+$("#takenking").hover(fadeOutAnimation, fadeInAnimation)
+//              Old code 
+//                  |
+//                  V
+//$("#takenking").hover(function () {
+//     $(this).stop();
+//     $(this).fadeOut(300);
+// }, function () {
+//     $(this).stop();
+//     $(this).fadeIn(170)
+// });
 
 $("#textHider").click(function () {
     $(".text").toggle()
     $("#thrallBox").toggle()
 });
 
-$('#nameThrall').bind('keypress', function (e) {
-    if (e.which === 13) {
-        let thrName = $("#nameThrall").val();
-        $("#thrallTitle").text(thrName);
-        $("#nameThrall").val("");
-    }
-});
+$('#nameThrall').bind('keypress', enterToSubmit);
+//               Old code 
+//                  |
+//                  V
+// $('#nameThrall').bind('keypress', function (e) {
+//     if (e.which === 13) {
+//         let thrName = $("#nameThrall").val();
+//         $("#thrallTitle").text(thrName);
+//         $("#nameThrall").val("");
+//     }
+// });
 
-function moveThrall() {$(document).mousemove(function(event) {
-  $("#thrallMover").css({
-    left: event.pageX - 270,
-    top: event.pageY - 300,
-    position: "absolute"
-  })
-}
-)};
-
-$("#thrall").dblclick(moveThrall)
+$("#thrall").dblclick(moveThrall);
--- garden/creature.js
+++ garden/creature.js
@@ -9,6 +9,7 @@ let allCreatures = []
 //prnt single creature on a page
 //add creature to the memory
 //reset the form
+
 function getCreatureFromForm() {
 
     const freshCreature = {
@@ -20,51 +21,101 @@ function getCreatureFromForm() {
 
     return freshCreature;
 };
-function renderCreature(creature) {
-  let crEyesHTML = "";
-for (let i = 0; i < creature.eyesNum; i++) {
-    crEyesHTML = crEyesHTML + "<div class='eye'></div>";
+
+async function getRandomName() {
+
+    const response = await fetch(
+        "https://api.gofakeit.com/funcs/petname",
+        { method: "GET", });
+
+    const nameRandom = await response.text();
+    console.log("Got name:", nameRandom);
+    return nameRandom;
 };
-const html =
-    `<div class="creature">
+
+async function getRandomColor() {
+
+    const response = await fetch(
+        "https://api.gofakeit.com/funcs/hexcolor",
+        { method: "GET", });
+
+    const colorRandom = await response.text();
+    console.log("Got color:", colorRandom);
+    return colorRandom;
+};
+
+async function randomizeCreature() {
+    const eyesRandom = Math.floor(Math.random() * 5) + 1;
+    const nameRandom = await getRandomName();
+    const colorRandom = await getRandomColor();
+    const sizeRandom = "Large";
+
+    const randomCreature = {
+        name: nameRandom,
+        color: colorRandom,
+        size: sizeRandom,
+        eyeNum: eyesRandom
+    };
+    console.log("Creature here", randomCreature)
+    return randomCreature;
+
+};
+
+function renderCreature(creature) {
+    let crEyesHTML = "";
+    for (let i = 0; i < creature.eyesNum; i++) {
+        crEyesHTML = crEyesHTML + "<div class='eye'></div>";
+    };
+    const html =
+        `<div class="creature">
                 <div class = ${creature.size} style="background-color: ${creature.color}">${crEyesHTML}
                 </div>
                 <div class="creature-info">
                 ${creature.name}
                 </div>
             </div>`;
-            return html;
-}
+            console.log(html);
+            console.log(creature);
+    return html;
+};
 
 function addCreatureToDOM(creature) {
-  const html = renderCreature(creature);
-  $("#creatureList").append(html);
-  debugger;
-}
+    const html = renderCreature(creature);
+    $("#creatureList").append(html);
+};
 
 function isCreatureValid(creature) {
     if (creature.name === "") return false;
     if (creature.name.length > 12) return false;
     if (isNaN(creature.eyesNum) || creature.eyesNum > 5) return false;
     return true;
-}
+};
 
 function clearForm() {
     $("#crName").val("")
     $("#crColor").val("#68e0e8")
     $("#crEyesNum").val("")
     $("#crSize").val("")
-}
+};
 
 $("#summon").click(
-    function () {
+    async function () {
 
-        const newCreature = getCreatureFromForm();
-        //do checks, exit function if something is wrong
-        if (isCreatureValid(newCreature) == false) {
-        return;
+        let newCreature;
+        if ($("#crRandom").is(':checked')) {
+            newCreature = await randomizeCreature();
         }
-        addCreatureToDOM(newCreature);
-    });
+        else {
+            newCreature = getCreatureFromForm();
+        }
+        if (isCreatureValid(newCreature) == false) {
+            return;
+        }
 
+        allCreatures.push(newCreature);
+        addCreatureToDOM(newCreature);
+        clearForm();
+        debugger;
+    });
     
+
